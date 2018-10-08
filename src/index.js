1cbf762f4ac2011fe9e35f5214630f765c0bf3b6
Dragos <dragos.giugula@gmail.com>
2018-10-08 16:06:28 +0200

    new component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import './Resources/css/app.css';
+import Routes from './Routes'
+
+
+import {BroswerRouter} from 'react-router-dom';
+import Routes from './Routes';
+
+const App = () => {
+  
+    return(
+      <BroswerRouter>
+      <Routes/>
+
+      </BroswerRouter>
+    )
+}
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
