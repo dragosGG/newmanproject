311329c4b3fb9b6f48fc7a4fda1f8e98563dc6e9
Dragos <dragos.giugula@gmail.com>
2018-10-16 12:41:30 +0200

    header bugs

--- src/Routes.js
+++ src/Routes.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import Layout from './Hoc/Layout';
 
+import {Switch, Route} from 'react-router-dom';
+import Home from './Components/Home'
+
 const Routes = (props) => {
-  return(
+  return (
     <Layout>
-      second line
+      <Switch>
+          <Route exact component={Home} path="/"/>
+
+      </Switch>
     </Layout>
   )
 }
-
+ 
 export default Routes;
