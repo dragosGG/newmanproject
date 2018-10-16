60ac00cb454623196768de4ad57ccafd5c3f354f
Dragos <dragos.giugula@gmail.com>
2018-10-16 12:52:41 +0200

    still typo bugs

--- src/Components/Header_Footer/Header.js
+++ src/Components/Header_Footer/Header.js
@@ -1,55 +1,46 @@
 import React, { Component } from 'react';
-import AppBar from '@material-ui/core/AppBar'
-import ToolBar from '@material-ui/core/Toolbar';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar'
 import Button from '@material-ui/core/Button';
-import {Link} from 'react-router-dom';
-import {CityLogo} from '../ui/icons';
 
+import { Link } from 'react-router-dom';
 
-
+import { CityLogo } from '../ui/icons';
 
 class Header extends Component {
     render() {
         return (
             <AppBar
-            position="fixed"
-            style={{
-                backgroundColor:'#98c5e9',
-                boxShadow:'none',
-                padding:'10px 0',
-                borderBottom: '2px solid #00285e'
-            }}
+                position="fixed"
+                style={{
+                    backgroundColor:'#98c5e9',
+                    boxShadow: 'none',
+                    padding:'10px 0',
+                    borderBottom: '2px solid #00285e'
+                }}
             >
-            <ToolBar style={{display:'flex'}}>
-            <div style={{flexGrow:1}}>
-            <div className="header_logo">
-                    <CityLogo
-                    link={true}
-                    linkTo={"/"}
-                    width="70x"
-                    height="70px"
-                    />
-            </div>
-            </div>
+                <Toolbar style={{display:'flex'}}>
+                    <div style={{flexGrow: 1}}>
+                        <div className="header_logo">
+                            <CityLogo
+                                link={true}
+                                linkTo="/"
+                                width="70px"
+                                height="70px"
+                            />
+                        </div>  
+                    </div>
 
-           <Link to="/the_team">
-            <Button color="inherit ">
-           The Team 
+                    <Link to="/the_team">
+                        <Button color="inherit">The team</Button>
+                    </Link>
+                    <Link to="/the_matches">
+                        <Button color="inherit">Matches</Button>
+                    </Link>
 
-           </Button>
-           
-           </Link>
-           <Button
-           colour="inherit ">
-           Matches 
-
-           </Button>
-
-
-            </ToolBar>
-
-
-            </AppBar>        );
+                </Toolbar>
+            </AppBar>
+        );
     }
 }
 
